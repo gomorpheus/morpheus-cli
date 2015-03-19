54c7fd93d67ec6849f1fcdb4df94bca4f019fe24
David Estes <davydotcom@gmail.com>
2015-03-19 09:37:26 -0400

    first commit

---
+++ lib/morpheus/cli.rb
@@ -0,0 +1,7 @@
+require "morpheus/cli/version"
+
+module Morpheus
+  module Cli
+    # Your code goes here...
+  end
+end
---
+++ lib/morpheus/cli/version.rb
@@ -0,0 +1,5 @@
+module Morpheus
+  module Cli
+    VERSION = "0.0.1"
+  end
+end
