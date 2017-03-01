82ba2705df055ddc3e5f7c7c49c2c055b64ac51e
James Dickson <jdickson@bcap.com>
2017-03-01 16:59:30 -0500

    tweak OptionParser.to_s

--- lib/morpheus/cli/option_parser.rb
+++ lib/morpheus/cli/option_parser.rb
@@ -7,12 +7,8 @@ module Morpheus
     # not used yet, maybe ever =o
     class Morpheus::Cli::OptionParser < OptionParser
 
-      attr_reader :footer
+      attr_accessor :footer
       
-      def footer=(msg)
-        @footer = msg
-      end
-
       alias :original_to_s :to_s
 
       def to_s
@@ -21,10 +17,20 @@ module Morpheus
 
       def full_help_message
         out = ""
-        out << original_to_s
+        #out << original_to_s
+        if banner
+          out << "#{banner}".sub(/\n?\z/, "\n")
+        end
+        if !self.to_a.empty?
+          #out << "Options:\n"
+          out << summarize().join("")
+        end
         if footer
-          out << footer.to_s
+          # nice_footer = footer.split("\n").collect {|line| "#{summary_indent}#{line}" }.join("\n")
+          nice_footer = footer
           out << "\n"
+          out << "#{nice_footer}".sub(/\n?\z/, "\n")
+          # out << "\n"
         end
         out
       end
