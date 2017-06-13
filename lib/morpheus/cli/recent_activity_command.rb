bc43138ec40fca91505e90a4d02c92cef17c57a6
James Dickson <jdickson@bcap.com>
2017-06-13 11:27:40 -0400

    recent-activity command

--- lib/morpheus/cli/recent_activity_command.rb
+++ lib/morpheus/cli/recent_activity_command.rb
@@ -8,7 +8,6 @@ class Morpheus::Cli::RecentActivityCommand
   include Morpheus::Cli::AccountsHelper
 
   set_command_name :'recent-activity'
-  set_command_hidden # remove once this is done
 
   def initialize()
     # @appliance_name, @appliance_url = Morpheus::Cli::Remote.active_appliance
@@ -37,7 +36,7 @@ class Morpheus::Cli::RecentActivityCommand
       opts.on('--end TIMESTAMP','--end TIMESTAMP', "End timestamp. Default is now.") do |val|
         options[:end] = parse_time(val).iso8601
       end
-      build_common_options(opts, options, [:account, :list, :json, :dry_run])
+      build_common_options(opts, options, [:account, :list, :json, :yaml, :csv, :fields, :dry_run, :remote])
     end
     optparse.parse!(args)
     connect(options)
@@ -54,22 +53,90 @@ class Morpheus::Cli::RecentActivityCommand
       end
       json_response = @dashboard_interface.recent_activity(account_id, params)
       if options[:json]
+        if options[:include_fields]
+          json_response = {"activity" => filter_data(json_response["activity"], options[:include_fields]) }
+        end
         print JSON.pretty_generate(json_response)
         print "\n"
-      else
-
-        # todo: impersonate command and show that info here
-
-        print_h1 "Recent Activity"
-        print cyan
-        puts "Coming soon... see --json"
-        print reset,"\n"
-
+        return 0
       end
+      if options[:csv]
+        puts records_as_csv(json_response["activity"], options)
+        return 0
+      end
+      if options[:yaml]
+        if options[:include_fields]
+          json_response = {"activity" => filter_data(json_response["activity"], options[:include_fields]) }
+        end
+        puts as_yaml(json_response, options)
+        return 0
+      end
+
+      print_h1 "Recent Activity"
+      print cyan
+      items = json_response["activity"]
+      if items.empty?
+        puts "No activity found."
+        print reset,"\n"
+        return 0
+      end
+      # JD: this api response is funky, no meta and it includes date objects
+      items = items.select { |item| item['_id'] || item['name'] }
+      print_recent_activity_table(items, options)
+      print reset,"\n"
+      return 0
+
     rescue RestClient::Exception => e
       print_rest_exception(e, options)
-      exit 1
+      return 1
     end
+  end
+
+  def print_recent_activity_table(items, opts={})
+    columns = [
+      # {"ID" => lambda {|item| item['id'] } },
+      # {"SEVERITY" => lambda {|item| format_activity_severity(item['severity']) } },
+      {"TYPE" => lambda {|item| item['activityType'] } },
+      {"AUTHOR" => lambda {|item| item['userName'] || '' } },
+      {"MESSAGE" => lambda {|item| item['message'] || '' } },
+      # {"NAME" => lambda {|item| item['name'] } },
+      {"OBJECT" => lambda {|item| format_activity_display_object(item) } },
+      {"WHEN" => lambda {|item| format_local_dt(item['ts']) } }
+      # {"WHEN" => lambda {|item| "#{format_duration(item['ts'])} ago" } }
+    ]
+    if opts[:include_fields]
+      columns = opts[:include_fields]
+    end
+    print as_pretty_table(items, columns, opts)
+  end
+
+  def format_activity_severity(severity, return_color=cyan)
+    out = ""
+    status_string = severity
+    if status_string == 'critical'
+      out << "#{red}#{status_string.capitalize}#{return_color}"
+    elsif status_string == 'warning'
+      out << "#{yellow}#{status_string.capitalize}#{return_color}"
+    elsif status_string == 'info'
+      out << "#{cyan}#{status_string.capitalize}#{return_color}"
+    else
+      out << "#{cyan}#{status_string}#{return_color}"
+    end
+    out
+  end
+
+  def format_activity_display_object(item)
+    out = ""
+    if item['name']
+      out << item['name']
+    end
+    if item['objectType']
+      out << " (#{item['objectType']} #{item['objectId']})"
+    end
+    if item['deleted']
+      out << " [deleted]"
+    end
+    out
   end
 
 end
