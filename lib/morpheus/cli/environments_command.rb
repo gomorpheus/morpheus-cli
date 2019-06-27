c6bb16b768345cfefb7128573b31cff8e5f32e17
James Dickson <jdickson@bcap.com>
2019-06-27 12:49:25 -0400

    tweak no environments yellow msg

--- lib/morpheus/cli/environments_command.rb
+++ lib/morpheus/cli/environments_command.rb
@@ -52,7 +52,7 @@ class Morpheus::Cli::EnvironmentsCommand
         subtitles += parse_list_subtitles(options)
         print_h1 title, subtitles
         if environments.empty?
-          puts yellow,"No Environments found.",reset
+          print yellow,"No Environments found.",reset
         else
           print_environments_table(environments)
           print_results_pagination(json_response)
