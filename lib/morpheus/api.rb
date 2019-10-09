47e8e49348f29d66b05d47b2e2ec67a82a4eefbe
James Dickson <jdickson@bcap.com>
2019-10-09 14:39:05 -0400

    require morpheus/api for lighter loading of interface classes only

---
+++ lib/morpheus/api.rb
@@ -0,0 +1,10 @@
+require "morpheus/cli/version"
+require 'morpheus/cli/command_error'
+require "morpheus/rest_client"
+require 'morpheus/formatters'
+#require 'morpheus/logging'
+#require 'term/ansicolor'
+
+# load interfaces
+require 'morpheus/api/api_client.rb'
+Dir[File.dirname(__FILE__)  + "/api/**/*.rb"].each {|file| load file }
