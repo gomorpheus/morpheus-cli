3ccf6d75c03ff1889057f2b0915ee5c4a7712386
James Dickson <jdickson@bcap.com>
2020-02-12 17:02:44 -0500

    use the new /api/library/instance-types url

--- lib/morpheus/api/library_instance_types_interface.rb
+++ lib/morpheus/api/library_instance_types_interface.rb
@@ -10,21 +10,21 @@ class Morpheus::LibraryInstanceTypesInterface < Morpheus::APIClient
 
   def get(id, params={})
     raise "#{self.class}.get() passed a blank id!" if id.to_s == ''
-    url = "#{@base_url}/api/library/#{id}"
+    url = "#{@base_url}/api/library/instance-types/#{id}"
     headers = { params: params, authorization: "Bearer #{@access_token}" }
     opts = {method: :get, url: url, headers: headers}
     execute(opts)
   end
 
   def list(params={})
-    url = "#{@base_url}/api/library"
+    url = "#{@base_url}/api/library/instance-types"
     headers = { params: params, authorization: "Bearer #{@access_token}" }
     opts = {method: :get, url: url, headers: headers}
     execute(opts)
   end
 
   def create(options)
-    url = "#{@base_url}/api/library"
+    url = "#{@base_url}/api/library/instance-types"
     headers = { :authorization => "Bearer #{@access_token}", 'Content-Type' => 'application/json' }
     payload = options
     opts = {method: :post, url: url, headers: headers, payload: payload.to_json}
@@ -32,7 +32,7 @@ class Morpheus::LibraryInstanceTypesInterface < Morpheus::APIClient
   end
 
   def update(id, options)
-    url = "#{@base_url}/api/library/#{id}"
+    url = "#{@base_url}/api/library/instance-types/#{id}"
     headers = { :authorization => "Bearer #{@access_token}", 'Content-Type' => 'application/json' }
     payload = options
     opts = {method: :put, url: url, headers: headers, payload: payload.to_json}
@@ -40,14 +40,14 @@ class Morpheus::LibraryInstanceTypesInterface < Morpheus::APIClient
   end
 
   def toggle_featured(id, params={}, payload={})
-    url = "#{@base_url}/api/library/#{id}/toggle-featured"
+    url = "#{@base_url}/api/library/instance-types/#{id}/toggle-featured"
     headers = { :params => params, :authorization => "Bearer #{@access_token}", 'Content-Type' => 'application/json' }
     opts = {method: :put, url: url, headers: headers, payload: payload.to_json}
     execute(opts)
   end
 
   def destroy(id, payload={})
-    url = "#{@base_url}/api/library/#{id}"
+    url = "#{@base_url}/api/library/instance-types/#{id}"
     headers = { :authorization => "Bearer #{@access_token}", 'Content-Type' => 'application/json' }
     opts = {method: :delete, url: url, headers: headers, payload: payload.to_json}
     execute(opts)
@@ -55,7 +55,7 @@ class Morpheus::LibraryInstanceTypesInterface < Morpheus::APIClient
 
   # NOT json, multipart file upload
   def update_logo(id, logo_file)
-    url = "#{@base_url}/api/library/#{id}/update-logo"
+    url = "#{@base_url}/api/library/instance-types/#{id}/update-logo"
     headers = { :params => {}, :authorization => "Bearer #{@access_token}"}
     payload = {}
     payload[:logo] = logo_file
