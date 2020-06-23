5e5814463649e8f7bf967a33e3be858ed7a45928
James Dickson <jdickson@bcap.com>
2020-06-23 11:39:12 -0400

    removed unused AuthCommand

--- lib/morpheus/cli.rb
+++ lib/morpheus/cli.rb
@@ -77,7 +77,6 @@ module Morpheus
       load 'morpheus/cli/whoami.rb'
       load 'morpheus/cli/access_token_command.rb'
       load 'morpheus/cli/user_settings_command.rb'
-      # load 'morpheus/cli/auth_command.rb'
       load 'morpheus/cli/dashboard_command.rb'
       load 'morpheus/cli/recent_activity_command.rb' # deprecated, removing soon
       load 'morpheus/cli/activity_command.rb'
