d6c8eed6ee76ed34dc0863a8678fed88993aaee6
James Dickson <jdickson@bcap.com>
2020-10-06 07:55:27 -0400

    search --go

--- lib/morpheus/cli/search_command.rb
+++ lib/morpheus/cli/search_command.rb
@@ -1,12 +1,10 @@
 require 'morpheus/cli/cli_command'
-require 'yaml'
 
 class Morpheus::Cli::SearchCommand
   include Morpheus::Cli::CliCommand
-  include Morpheus::Cli::DeploymentsHelper
   
   set_command_name :search
-  set_command_description "Global search for finding all types of things"
+  set_command_description "Global search for finding all types of objects"
 
   def connect(opts)
     @api_client = establish_remote_appliance_connection(opts)
@@ -16,13 +14,17 @@ class Morpheus::Cli::SearchCommand
   def handle(args)
     options = {}
     params = {}
-    ref_ids = []
     optparse = Morpheus::Cli::OptionParser.new do |opts|
       opts.banner = "Usage: #{prog_name} #{command_name} [phrase]"
+      opts.on("-g", "--go", "Go get details for the top search result instead of printing the list.") do
+        options[:go] = true
+      end
       build_standard_list_options(opts, options)
       opts.footer = <<-EOT
-Global search that provides a way to find records of all types that match a phrase.
-[phrase] is required. This is the phrase to search for, the name of an object usually.
+Global search for finding all types of objects
+[phrase] is required. This is the phrase to search for.
+Prints the list of search results with the most relevant first.
+or use the --go option to get details about the top result instead.
 EOT
     end
     optparse.parse!(args)
@@ -41,7 +43,17 @@ EOT
       return
     end
     json_response = @search_interface.list(params)
-    search_results = json_response["hits"] || json_response["results"]
+    search_results = json_response["hits"] || json_response["results"] || []
+    top_result = search_results[0]
+    if options[:go]
+      if top_result
+        print cyan,"Loading top search result: #{format_morpheus_type(top_result['type'])} #{top_result['name'] || top_result['id']} (score: #{top_result['score']})",reset,"\n"
+        return go_to_search_result(top_result, options)
+      else
+        # print cyan,"No results found.",reset,"\n"
+        raise_command_error "No search results for phrase '#{options[:phrase]}'"
+      end
+    end
     render_response(json_response, options, "hits") do
       print_h1 "Morpheus Search", parse_list_subtitles(options), options
       if search_results.empty?
@@ -79,10 +91,92 @@ EOT
       "Host"
     elsif val == "ComputeServerGroup"
       "Cluster"
+    elsif val == "ComputeZonePool"
+      "Pool"
     else
       val
     end
   end
   
+  def go_to_search_result(result, options)
+    result_type = result['type']
+    cmd_class = lookup_morpheus_command(result['type'])
+    if cmd_class
+      get_options = []
+      get_options += (options[:remote] ? ["-r",options[:remote]] : [])
+      get_options += (options[:json] ? ["--json"] : [])
+      get_options += (options[:yaml] ? ["--yaml"] : [])
+      return cmd_class.new.handle(["get", result['id']] + get_options)
+    else
+      raise_command_error "Sorry, result cannot be loaded. type: #{result_type}, id: #{result['id']}, name: #{result['name']}"
+    end
+  end
+
+
+  def lookup_morpheus_command(result_type)
+    case result_type.to_s.downcase
+    when "computezone","cloud","zone"
+      Morpheus::Cli::Clouds
+    when "computesite","group"
+      Morpheus::Cli::Groups
+    when "computeserver","server","host"
+      Morpheus::Cli::Hosts
+    when "computeservergroup","serverGroup","cluster"
+      Morpheus::Cli::Clusters
+    when "instance"
+      Morpheus::Cli::Instances
+    when "app"
+      Morpheus::Cli::Apps
+    when "container"
+      Morpheus::Cli::Containers
+    when "computezonefolder","zonefolder","resourcefolder"
+      # crap, need result.zoneId, or resource-folders should not require cloud actually, get by id
+      # Morpheus::Cli::CloudFoldersCommand
+      nil
+    when "computezonepool","zonepool","resourcepool"
+      # crap, need result.zoneId, or resource-pools should not require cloud actually, get by id
+      # Morpheus::Cli::CloudResourcePoolsCommand
+      nil
+    # when "chassis"
+    #   Morpheus::Cli::ChassisCommand
+    when "network"
+      Morpheus::Cli::NetworksCommand
+    when "networkgroup"
+      Morpheus::Cli::NetworkGroupsCommand
+    when "networkpool"
+      Morpheus::Cli::NetworkPoolsCommand
+    when "networkdomain"
+      Morpheus::Cli::NetworkDomainsCommand
+    when "virtualimage"
+      Morpheus::Cli::VirtualImages
+    when "loadbalancer"
+      Morpheus::Cli::LoadBalancers
+    # when "virtualserver","loadbalancerinstance"
+    #   Morpheus::Cli::LoadBalancerInstances
+    when "instancetype"
+      # Morpheus::Cli::LibraryInstanceTypesCommand
+      Morpheus::Cli::LibraryInstanceTypes
+    when "instancetypelayout","layout"
+      Morpheus::Cli::LibraryLayoutsCommand
+    when "certificate"
+      # todo: WHAT! didnt I write certs already!?
+      Morpheus::Cli::CertificatesCommand
+    when "keypair"
+      Morpheus::Cli::KeyPairs
+    when "integration"
+      Morpheus::Cli::IntegrationsCommand
+    when "account","tenant"
+      Morpheus::Cli::TenantsCommand
+    when "user"
+      Morpheus::Cli::Users
+    when "role"
+      Morpheus::Cli::Roles
+    when "wikipage","wiki"
+      Morpheus::Cli::WikiCommand
+    else
+      nil
+    end
+  end
+
 end
 
