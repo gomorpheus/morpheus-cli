d18f03d615a654e7a4fe683d517f08bccd7ba51c
James Dickson <dickson.james@gmail.com>
2020-11-05 14:49:46 -0500

    grooming, format_app_status move to helper

--- lib/morpheus/cli/apps.rb
+++ lib/morpheus/cli/apps.rb
@@ -2185,26 +2185,6 @@ EOT
     print reset
   end
 
-  def format_app_status(app, return_color=cyan)
-    out = ""
-    status_string = app['status'] || app['appStatus'] || ''
-    if status_string == 'running'
-      out = "#{green}#{status_string.upcase}#{return_color}"
-    elsif status_string == 'provisioning'
-      out = "#{cyan}#{status_string.upcase}#{cyan}"
-    elsif status_string == 'stopped' or status_string == 'failed'
-      out = "#{red}#{status_string.upcase}#{return_color}"
-    elsif status_string == 'unknown'
-      out = "#{yellow}#{status_string.upcase}#{return_color}"
-    elsif status_string == 'warning' && app['instanceCount'].to_i == 0
-      # show this instead of WARNING
-      out =  "#{cyan}EMPTY#{return_color}"
-    else
-      out =  "#{yellow}#{status_string.upcase}#{return_color}"
-    end
-    out
-  end
-
   def format_app_tiers(app)
     out = ""
     begin
