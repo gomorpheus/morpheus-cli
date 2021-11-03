a97a31f62449ab386d300ff2e67f67e0a4dd3b06
Dan DeVilbiss <ddevilbiss@bcap.com>
2021-11-03 13:55:51 -0700

    disabling network scopes

--- lib/morpheus/cli.rb
+++ lib/morpheus/cli.rb
@@ -193,7 +193,7 @@ module Morpheus
       load 'morpheus/cli/vdi_gateways_command.rb'
       load 'morpheus/cli/vdi_command.rb' # (VDI persona)
       load 'morpheus/cli/certificates_command.rb' # (VDI persona)
-      load 'morpheus/cli/network_scopes_command.rb'
+      #load 'morpheus/cli/network_scopes_command.rb'
       # add new commands here...
 
     end
