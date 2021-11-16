45cd7f319630febfccaf0369dcddcfe48f006f1b
James Dickson <dickson.james@gmail.com>
2021-11-16 12:07:48 -0500

    access-token details should print Remote

--- lib/morpheus/cli/commands/access_token_command.rb
+++ lib/morpheus/cli/commands/access_token_command.rb
@@ -75,7 +75,7 @@ class Morpheus::Cli::AccessTokenCommand
       "Refresh Token" => lambda {|wallet| wallet['refresh_token'] },
       "Login Date" => lambda {|wallet| format_local_dt(wallet['login_date']) },
       "Expire Date" => lambda {|wallet| wallet['expire_date'] ? format_local_dt(wallet['expire_date']) : "" },
-      # "Remote" => lambda {|wallet| display_appliance(@appliance_name, @appliance_url) },
+      "Remote" => lambda {|wallet| display_appliance(@appliance_name, @appliance_url) },
     }
     print cyan
     puts as_description_list(@wallet, description_cols)
