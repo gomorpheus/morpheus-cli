7266e8a0a4026291d96803c2457b8429da0eac10
James Dickson <dickson.james@gmail.com>
2022-04-06 10:19:48 -0400

    instances prepare_apply changed to GET

--- lib/morpheus/api/instances_interface.rb
+++ lib/morpheus/api/instances_interface.rb
@@ -379,10 +379,10 @@ class Morpheus::InstancesInterface < Morpheus::APIClient
     execute(opts)
   end
 
-  def prepare_apply(id, params={}, payload={})
+  def prepare_apply(id, params={})
     url = "#{@base_url}/api/instances/#{id}/prepare-apply"
     headers = {:params => params, :authorization => "Bearer #{@access_token}", 'Content-Type' => 'application/json' }
-    opts = {method: :post, url: url, headers: headers, payload: payload.to_json}
+    opts = {method: :get, url: url, headers: headers, payload: payload.to_json}
     execute(opts)
   end
 
