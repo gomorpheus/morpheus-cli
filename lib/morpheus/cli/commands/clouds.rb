cb6e3c29ae9e0efb7ec7072c49df1a352f2b9700
Dan DeVilbiss <ddevilbiss@bcap.com>
2022-07-19 12:52:14 -0700

    adding credential option to clouds for 5.4 compatibility

--- lib/morpheus/cli/commands/clouds.rb
+++ lib/morpheus/cli/commands/clouds.rb
@@ -985,6 +985,10 @@ class Morpheus::Cli::Clouds
     # Details (zoneType.optionTypes)
 
     if cloud_type && cloud_type['optionTypes']
+      if !cloud_type['optionTypes'].find {|opt| opt['type'] == 'credential'}
+        tmp_option_types << {'fieldName' => 'type', 'fieldLabel' => 'Credentials', 'type' => 'credential', 'optionSource' => 'credentials', 'required' => true, 'defaultValue' => 'local', 'config' => {'credentialTypes' => ['username-password']}, 'displayOrder' => 7}
+        cloud_type['optionTypes'].select {|opt| ['username', 'password'].include?(opt['fieldName'])}.each {|opt| opt['localCredential'] = true}
+      end
       # adjust displayOrder to put these at the end
       #tmp_option_types = tmp_option_types + cloud_type['optionTypes']
       cloud_type['optionTypes'].each do |opt|
