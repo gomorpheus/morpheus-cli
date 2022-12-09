0c15c66f4064a7a2ff7beb83c1e3f70eecedddca
James Dickson <jdickson@morpheusdata.com>
2022-12-09 18:10:53 -0500

    policies add type option value should be code, not id

--- lib/morpheus/cli/commands/policies_command.rb
+++ lib/morpheus/cli/commands/policies_command.rb
@@ -334,7 +334,7 @@ class Morpheus::Cli::PoliciesCommand
         options['eachUser'] = val.to_s == 'on' || val.to_s == 'true' || val.to_s.empty?
       end
       
-      opts.on('-t', '--type ID', "Policy Type Name or ID") do |val|
+      opts.on('-t', '--type ID', "Policy Type Name, Code or ID") do |val|
         options['type'] = val
       end
       opts.on('--name VALUE', String, "Name for this policy") do |val|
@@ -417,7 +417,7 @@ class Morpheus::Cli::PoliciesCommand
           print_red_alert "No available policy types found!"
           return 1
         end
-        policy_types_dropdown = available_policy_types.collect {|it| {'name' => it['name'], 'value' => it['id']} }
+        policy_types_dropdown = available_policy_types.collect {|it| {'name' => it['name'], 'value' => it['code'], 'id' => it['id']} }
         policy_type_id = nil
         policy_type = nil
         if options['type']
