d78d53dd5ac8f1e6ffe395262a59d9582e1a56cf
James Dickson <jdickson@morpheusdata.com>
2024-10-25 16:00:23 -0400

    process retry and cancel

--- lib/morpheus/api/processes_interface.rb
+++ lib/morpheus/api/processes_interface.rb
@@ -2,8 +2,12 @@ require 'morpheus/api/api_client'
 
 class Morpheus::ProcessesInterface < Morpheus::APIClient
 
+  def base_path
+    "/api/processes"
+  end
+
   def list(params={})
-    url = "#{@base_url}/api/processes"
+    url = base_path
     headers = { params: params, authorization: "Bearer #{@access_token}" }
     opts = {method: :get, url: url, headers: headers}
     execute(opts)
@@ -11,18 +15,26 @@ class Morpheus::ProcessesInterface < Morpheus::APIClient
 
   def get(id, params={})
     raise "#{self.class}.get() passed a blank id!" if id.to_s == ''
-    url = "#{@base_url}/api/processes/#{id}"
+    url = "#{base_path}/#{CGI::escape(id.to_s)}"
     headers = { params: params, authorization: "Bearer #{@access_token}" }
     opts = {method: :get, url: url, headers: headers}
     execute(opts)
   end
 
-  def get_event(id, params={})
-    raise "#{self.class}.get() passed a blank id!" if id.to_s == ''
-    url = "#{@base_url}/api/processes/events/#{id}"
+  def get_event(event_id, params={})
+    raise "#{self.class}.get() passed a blank event id!" if event_id.to_s == ''
+    url = "#{base_path}/events/#{CGI::escape(event_id.to_s)}"
     headers = { params: params, authorization: "Bearer #{@access_token}" }
     opts = {method: :get, url: url, headers: headers}
     execute(opts)
+  end
+
+  def retry(id, payload={}, params={}, headers={})
+    execute(method: :post, url: "#{base_path}/#{CGI::escape(id.to_s)}/retry", params: params, payload: payload, headers: headers)
+  end
+
+  def cancel(id, payload={}, params={}, headers={})
+    execute(method: :post, url: "#{base_path}/#{CGI::escape(id.to_s)}/cancel", params: params, payload: payload, headers: headers)
   end
 
 end
