66d4b1d1dcbe5497339c0c05b220aeb555fed460
James Dickson <jdickson@morpheusdata.com>
2025-01-09 10:50:51 -0500

    Updated roles unit test to fetch first role instead of System Admin, in case test user is sub role

--- test/cli/roles_test.rb
+++ test/cli/roles_test.rb
@@ -7,9 +7,22 @@ class MorpheusTest::RolesTest < MorpheusTest::TestCase
     assert_execute %(roles list "System Admin")
   end
 
+  # def test_roles_get_system_admin
+  #   assert_execute %(roles get "System Admin")
+  #   assert_execute %(roles get "System Admin" --permissions)
+  # end
+
   def test_roles_get
-    assert_execute %(roles get "System Admin")
-    assert_execute %(roles get "System Admin" --permissions)
+    # role = client.roles.list({})['roles'][0]
+    role = client.roles.list({})['roles'].find {|r| r['authority'] !~ /\A\d+\Z/}
+    if role
+      assert_execute %(roles get "#{role['id']}")
+      name_arg = role['authority']
+      assert_execute %(roles get "#{escape_arg name_arg}")
+      assert_execute %(roles get "#{escape_arg name_arg}" --permissions)
+    else
+      puts "No role found, unable to execute test `#{__method__}`"
+    end
   end
 
   def test_roles_list_permissions
