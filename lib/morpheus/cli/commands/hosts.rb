99a42684bad6f853fbbf21e245553e541de0cbcc
James Dickson <jdickson@morpheusdata.com>
2025-03-21 10:32:54 -0400

    Enhance hosts get [name] multiple servers error to print list of servers

--- lib/morpheus/cli/commands/hosts.rb
+++ lib/morpheus/cli/commands/hosts.rb
@@ -2440,7 +2440,9 @@ EOT
       print_red_alert "Server not found by name #{name}"
       exit 1
     elsif results['servers'].size > 1
-      print_red_alert "Multiple Servers exist with the name #{name}. Try using id instead"
+      print_red_alert "Multiple Servers exist with the name '#{name}'"
+      puts_error as_pretty_table(results['servers'], [:id, :name], {color:red})
+      print_red_alert "Try using ID instead"
       exit 1
     end
     return results['servers'][0]
