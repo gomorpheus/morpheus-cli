71927e038f6808857d2cadd911cce340789f2901
Alex Clement <MrClement@users.noreply.github.com>
2025-05-20 10:07:48 -0600

    Don't prompt for root datastore when provision type has disableRootDatastore set to true

--- lib/morpheus/cli/mixins/provisioning_helper.rb
+++ lib/morpheus/cli/mixins/provisioning_helper.rb
@@ -1187,8 +1187,8 @@ module Morpheus::Cli::ProvisioningHelper
       #volume['size'] = plan_size
       #volume['sizeId'] = nil #volume.delete('sizeId')
     end
-    
-    if !datastore_options.empty?
+
+    if !datastore_options.empty? && !provision_type['disableRootDatastore']
       default_datastore = datastore_options.find {|ds| ds['value'].to_s == volume['datastoreId'].to_s}
       v_prompt = Morpheus::Cli::OptionTypes.prompt([{'fieldContext' => field_context, 'fieldName' => 'datastoreId', 'type' => 'select', 'fieldLabel' => 'Root Datastore', 'selectOptions' => datastore_options, 'required' => true, 'description' => 'Choose a datastore.', 'defaultValue' => default_datastore ? default_datastore['name'] : volume['datastoreId']}], options[:options])
       volume['datastoreId'] = v_prompt[field_context]['datastoreId']
